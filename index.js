d335c96825f8b471c86f9e89a4eac17cef0a1bec
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-04-09 15:42:59 +0530

    1.0

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-    // Set the end time 3 hours from now
     var endTime = new Date();
-    endTime.setHours(endTime.getHours() + 3);
+    endTime.setHours(endTime.getHours() + 0);
     function real(){
         btn.style.display = 'none'
         var countdown = setInterval(function() {
-            // Get the current time
             var now = new Date().getTime();
-      
-            // Calculate the distance between now and the end time
             var distance = endTime - now;
-      
-            // Calculate hours, minutes and seconds left
             var hours = Math.floor(distance / (1000 * 60 * 60));
             var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      
-            // Display the countdown in the HTML element
             document.getElementById("countdown").innerHTML = hours + "h "
               + minutes + "m " + seconds + "s ";
-      
-            // If the countdown is over, stop the interval
             if (distance < 0) {
               clearInterval(countdown);
-              document.getElementById("countdown").innerHTML = "thank you";
+              document.getElementById("countdown").innerHTML = "thank you!!";
             }
           }, 1000);
     }
-    // Update the countdown every second
     var btn = document.getElementById("ST");
-    // add event listener for the button, for action "click"
     btn.addEventListener("click", real);
